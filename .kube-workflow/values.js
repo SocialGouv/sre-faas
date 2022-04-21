2de827add7e9c138ab8cda78ff0ae518186e4cde
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-04-21 16:02:21 +0200

    fix

---
+++ .kube-workflow/values.js
@@ -0,0 +1,23 @@
+const fs = require("fs");
+const path = require("path");
+
+const funcsDir = path.join(__dirname, "../functions");
+
+module.exports = (values) => {
+  const functionsNames = fs
+    .readdirSync(funcsDir)
+    .filter((name) => fs.statSync(path.join(funcsDir, name)).isDirectory());
+
+  const apps = {};
+  functionsNames.forEach((name) => {
+    apps[`app-${name}`] = {
+      namespace: `${values.global.namespace}-${name}`,
+      containerPort: 3000,
+      imagePackage: name,
+    };
+  });
+
+  return Object.assign(values, {
+    ...apps,
+  });
+};
