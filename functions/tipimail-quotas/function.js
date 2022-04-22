c134114ab045c2cba4259b9d26d61c29de312a81
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-04-22 11:03:29 +0200

    fix

--- functions/tipimail-quotas/function.js
+++ functions/tipimail-quotas/function.js
@@ -1,7 +1,36 @@
+const pAll = require("p-all");
 
+const getAccountCredits = async (account) => {
+  fetch("https://api.tipimail.com/v1/account", {
+    headers: {
+      "X-Tipimail-ApiUser": account.username,
+      "X-Tipimail-ApiKey": account.token,
+      "Content-Type": "application/json",
+      "Cache-Control": "no-cache",
+    },
+  })
+    .then((r) => r.json())
+    .then((data) => data.credits);
+};
+
+const getAccountsCredits = async (accounts) => {
+  const metrics = [
+    "# TYPE tipimail_credits gauge",
+    ...(await pAll(
+      accounts.map(
+        (account) => () =>
+          `tipimail_credits{account="${account.name}"} ${getAccountCredits(
+            account
+          )}`
+      )
+    ),
+    { concurrency: 1 }),
+  ];
+  return metrics.join("\n");
+};
 
 module.exports = async (req, res) => {
-  const accounts = process.env.TIPIMAIL_ACCOUNTS;
+  const accounts = JSON.parse(process.env.TIPIMAIL_ACCOUNTS);
   console.log(accounts);
-  return "io"
+  return getAccountsCredits(accounts);
 };
