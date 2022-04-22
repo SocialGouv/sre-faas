c3e2a8d6897b29efcf38b22657b97fcf5b012b12
Julien Bouquillon <julien.bouquillon@sg.social.gouv.fr>
2022-04-22 13:10:54 +0200

    fix

--- .kube-workflow/values.js
+++ .kube-workflow/values.js
@@ -4,6 +4,8 @@ const path = require("path");
 const funcsDir = path.join(__dirname, "../functions");
 
 module.exports = (values) => {
+  console.log("values", values);
+
   const functionsNames = fs
     .readdirSync(funcsDir)
     .filter((name) => fs.statSync(path.join(funcsDir, name)).isDirectory());
@@ -14,7 +16,18 @@ module.exports = (values) => {
     apps[`app-${name}`] = {
       containerPort: 3000,
       imagePackage: name,
+      host: values.global.isProd
+        ? `sre-faas-${name}.${values.global.domain}`
+        : values.global.isPreProd
+        ? `sre-faas-${name}-preprod.${values.global.domain}`
+        : `sre-faas-${name}-${values.global.branchSlug}.${values.global.domain}`,
     };
+    // todo: make it available through some values.yaml
+    if (name === "tipimail-quotas") {
+      apps[`app-${name}`].envFrom = [
+        { secretRef: { name: "tipimail-accounts" } },
+      ];
+    }
   });
 
   return Object.assign(values, {
